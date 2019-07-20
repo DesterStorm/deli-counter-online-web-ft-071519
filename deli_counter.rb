1bd9b7bbba57f6e5b9ae175e90e9cf83ec3554b4
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-20 23:45:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 # katz_deli = []
 
-# def line (array)
-#   if array.count == 0
-#       puts "The line is currently empty."
-#   else
-#       resp = "The line is currently:"
-#       array.each_with_index do |name, index|
-#         resp += " #{index+1}. #{name}" 
-#   end
-#   puts resp
-#   end
-# end
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    resp = "The line is currently: " 
+    array.each_with_index do |name, index|
+      resp += "#{index + 1}. #{name}"
+    end
+  end
+  puts resp
+end
 
 # def take_a_number (array, name)
 #   array.push(name)
